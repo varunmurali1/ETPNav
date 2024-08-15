fcf737cc6514034f7e893e8b2761236af4dc9004
Dexter <dexterong94@gmail.com>
2024-08-15 12:44:28 -0400

    Updated offline image extraction

--- precompute_img_features/extract_rgb_features.py
+++ precompute_img_features/extract_rgb_features.py
@@ -16,7 +16,6 @@ import copy
 from PIL import Image
 import time
 from progressbar import ProgressBar
-from transformers import Dinov2Backbone, AutoImageProcessor
 
 import torch
 import torch.nn.functional as F
@@ -26,6 +25,7 @@ try:
 except RuntimeError:
     pass
 
+from transformers import Dinov2Backbone, AutoImageProcessor
 import clip
 
 VIEWPOINT_SIZE = 36 # Number of discretized views from one viewpoint
@@ -52,11 +52,14 @@ def process_features(proc_id, out_queue, scanvp_list, args):
 
     # Set up PyTorch CNN model
     torch.set_grad_enabled(False)
-    #model, img_transforms, device = build_feature_extractor(args.model_name, args.checkpoint_file)
-    model = Dinov2Backbone.from_pretrained("facebook/dinov2-large").cuda().eval()
-    image_processor = AutoImageProcessor.from_pretrained("facebook/dinov2-large") # I think this just crops, we may need some other processing on the image size (multiple of patch size 14)
-
-    device = 'cuda'
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    if args.output_features == 'clip':
+        model, img_transforms, device = build_feature_extractor(args.model_name, args.checkpoint_file)
+    elif args.output_features == 'dino':
+        # get last 12 layers
+        model = Dinov2Backbone.from_pretrained("facebook/dinov2-base", out_indices=[-12, -11, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1]).cuda().eval()
+        image_processor = AutoImageProcessor.from_pretrained("facebook/dinov2-base")
+    num = 0
     with h5py.File(args.img_db, 'r') as f:
         for scan_id, viewpoint_id in scanvp_list:
             data = f[f'{scan_id}_{viewpoint_id}'][...].astype('uint8')
@@ -66,24 +69,31 @@ def process_features(proc_id, out_queue, scanvp_list, args):
                 image = Image.fromarray(image[:, :, ::-1]) #cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
                 images.append(image)
 
-            #images = torch.stack([img_transforms(image) for image in images], 0).to(device)
-            images = np.asarray(images)
-            images = image_processor(images, return_tensors='pt')['pixel_values'].to(device)
+            if args.output_features == 'clip':
+                images = torch.stack([img_transforms(image) for image in images], 0).to(device)
+            elif args.output_features == 'dino':
+                images = image_processor(images, return_tensors="pt")['pixel_values'].to(device)
             fts, logits = [], []
             for k in range(0, len(images), args.batch_size):
-                #b_fts = model.encode_image(images[k: k+args.batch_size])
-                b_fts = model(images[k: k+args.batch_size]).feature_maps[-1]
-                if b_fts.dim() == 4:
-                    b_fts = b_fts.mean((2,3))
-                assert b_fts.dim() == 2
-                # b_logits = model.head(b_fts)
+                if args.output_features == 'clip':
+                    patch_fts, cls_fts, layer_fts = model.encode_image(images[k: k+args.batch_size])
+                    layer_fts = layer_fts.permute(0, 1, 3, 2)
+                    layer_fts = layer_fts[:, :, :, 1:]
+                    b_fts = layer_fts # B, N, C, HW
+                elif args.output_features == 'dino':
+                    b_fts = []
+                    layer_fts = model(images[k: k+args.batch_size]).feature_maps
+                    layer_fts = [layer_ft.unsqueeze(0) for layer_ft in layer_fts]
+                    layer_fts = torch.cat(layer_fts, dim=0)
+                    layer_fts = layer_fts.permute(1, 0, 2, 3, 4)
+                    layer_fts = layer_fts.reshape(layer_fts.size(0), layer_fts.size(1), layer_fts.size(2), -1)
+                    b_fts = layer_fts  # B, N, C, HW
                 b_fts = b_fts.data.cpu().numpy()
-                # b_logits = b_logits.data.cpu().numpy()
                 fts.append(b_fts)
-                # logits.append(b_logits)
             fts = np.concatenate(fts, 0)
-            # logits = np.concatenate(logits, 0)
             out_queue.put((scan_id, viewpoint_id, fts, logits))
+            num += 1
+            print(num)
 
     out_queue.put(None)
 
@@ -92,53 +102,63 @@ def build_feature_file(args):
     os.makedirs(os.path.dirname(args.output_file), exist_ok=True)
 
     scanvp_list = load_viewpoint_ids(args.connectivity_dir)
-
-    num_workers = min(args.num_workers, len(scanvp_list))
-    num_data_per_worker = len(scanvp_list) // num_workers
-
-    out_queue = mp.Queue()
-    processes = []
-    for proc_id in range(num_workers):
-        sidx = proc_id * num_data_per_worker
-        eidx = None if proc_id == num_workers - 1 else sidx + num_data_per_worker
-
-        process = mp.Process(
-            target=process_features,
-            args=(proc_id, out_queue, scanvp_list[sidx: eidx], args)
-        )
-        process.start()
-        processes.append(process)
     
-    num_finished_workers = 0
-    num_finished_vps = 0
-
+    # Set up PyTorch CNN model
+    torch.set_grad_enabled(False)
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    if args.output_features == 'clip':
+        model, img_transforms, device = build_feature_extractor(args.model_name, args.checkpoint_file)
+    elif args.output_features == 'dino':
+        # get last 12 layers
+        model = Dinov2Backbone.from_pretrained("facebook/dinov2-base", out_indices=[-12, -11, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1]).cuda().eval()
+        image_processor = AutoImageProcessor.from_pretrained("facebook/dinov2-base")
+    
     progress_bar = ProgressBar(maxval=len(scanvp_list))
     progress_bar.start()
-
+    num_finished_vps = 0
+    
     with h5py.File(args.output_file, 'w') as outf:
-        while num_finished_workers < num_workers:
-            res = out_queue.get()
-            if res is None:
-                num_finished_workers += 1
-            else:
-                scan_id, viewpoint_id, fts, logits = res
+        with h5py.File(args.img_db, 'r') as f:
+            for scan_id, viewpoint_id in scanvp_list:
+                data = f[f'{scan_id}_{viewpoint_id}'][...].astype('uint8')
+                images = []
+                for i in range(VIEWPOINT_SIZE):
+                    image = data[i]
+                    image = Image.fromarray(image[:, :, ::-1]) #cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                    images.append(image)
+
+                if args.output_features == 'clip':
+                    images = torch.stack([img_transforms(image) for image in images], 0).to(device)
+                elif args.output_features == 'dino':
+                    images = image_processor(images, return_tensors="pt")['pixel_values'].to(device)
+                fts, logits = [], []
+                for k in range(0, len(images), args.batch_size):
+                    if args.output_features == 'clip':
+                        patch_fts, cls_fts, layer_fts = model.encode_image(images[k: k+args.batch_size])
+                        layer_fts = layer_fts.permute(0, 1, 3, 2)
+                        layer_fts = layer_fts[:, :, :, 1:]
+                        b_fts = layer_fts # B, N, C, HW
+                    elif args.output_features == 'dino':
+                        b_fts = []
+                        layer_fts = model(images[k: k+args.batch_size]).feature_maps
+                        layer_fts = [layer_ft.unsqueeze(0) for layer_ft in layer_fts]
+                        layer_fts = torch.cat(layer_fts, dim=0)
+                        layer_fts = layer_fts.permute(1, 0, 2, 3, 4)
+                        layer_fts = layer_fts.reshape(layer_fts.size(0), layer_fts.size(1), layer_fts.size(2), -1)
+                        b_fts = layer_fts  # B, N, C, HW
+                    b_fts = b_fts.data.cpu().numpy()
+                    fts.append(b_fts)
+                fts = np.concatenate(fts, 0)
+                data = fts
+                
                 key = '%s_%s'%(scan_id, viewpoint_id)
-                if args.out_image_logits:
-                    data = np.hstack([fts, logits])
-                else:
-                    data = fts
                 outf.create_dataset(key, data.shape, dtype='float32', compression='gzip')
                 outf[key][...] = data
                 outf[key].attrs['scanId'] = scan_id
                 outf[key].attrs['viewpointId'] = viewpoint_id
-
+                
                 num_finished_vps += 1
                 progress_bar.update(num_finished_vps)
-
-    progress_bar.finish()
-    for process in processes:
-        process.join()
-            
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -150,6 +170,7 @@ if __name__ == '__main__':
     parser.add_argument('--output_file', default='pretrain_src/img_features/Dinov2-views-habitat.hdf5')
     parser.add_argument('--batch_size', default=36, type=int)
     parser.add_argument('--num_workers', type=int, default=1)
+    parser.add_argument('--output_features', default=None, choices=['dino', 'clip'], required=True)
     args = parser.parse_args()
 
     build_feature_file(args)
